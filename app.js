9b836ba0b86e9a520f9c7d0bc0253a4d261ec240
Iris Leal <iris_leal_m@hotmail.com>
2020-02-18 12:59:02 -0800

    Graph now shows clicks and votes

--- app.js
+++ app.js
@@ -146,6 +146,9 @@ function displayVotationResults()
   displayCanvas();
 } //displayVotationResults
 
+/*
+This function returns the clicks for each position in the array allProductList, and returns an array.
+ */
 function getVotesInArray()
 {
   var arrVotes=[];
@@ -153,7 +156,20 @@ function getVotesInArray()
   {
     arrVotes.push(allProductList[i].timesClicked);
   }
-  return arrVotes
+  return arrVotes;
+}
+
+/*
+This function returns the number of render for each position in the array allProductList, and returns an array.
+ */
+function getNumRenderinArray()
+{
+  var arrRender=[];
+  for (var i=0; i<allProductList.length ; i++)
+  {
+    arrRender.push(allProductList[i].timesRendered);
+  }
+  return arrRender;
 }
 
 function displayCanvas()
@@ -168,6 +184,28 @@ function displayCanvas()
         label: '# of Votes',
         // data: [12, 19, 3, 5, 2, 3],
         data: getVotesInArray(),
+        backgroundColor: [
+          // 'rgba(255, 99, 132, 0.2)',
+          // 'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          // 'rgba(75, 192, 192, 0.2)',
+          // 'rgba(153, 102, 255, 0.2)',
+          // 'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          // 'rgba(255, 99, 132, 1)',
+          // 'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          // 'rgba(75, 192, 192, 1)',
+          // 'rgba(153, 102, 255, 1)',
+          // 'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Render',
+        // data: [12, 19, 3, 5, 2, 3],
+        data: getNumRenderinArray(),
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
